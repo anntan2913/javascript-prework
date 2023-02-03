0164b6550693065d5b2f70e4dead616dc0fd1fbb
Anna Tankielun <anna.r.tankielun.3@gmail.com>
2023-02-03 16:46:34 +0100

    add new variables in script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'nożyce';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to przegrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to jest remis!');
